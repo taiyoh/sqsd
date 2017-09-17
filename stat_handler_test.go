982108d142d0252c3c754301df38b37c8c86cdb7
taiyoh <sun.basix@gmail.com>
2017-09-17 16:18:13 +0900

    job summary関連のコントローラのテスト追加

--- stat_handler_test.go
+++ stat_handler_test.go
@@ -1,8 +1,12 @@
 package sqsd
 
 import (
+	"encoding/json"
+	"strconv"
 	"net/http"
 	"testing"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/aws/aws-sdk-go/aws"
 )
 
 func TestReqMethodValidate(t *testing.T) {
@@ -44,8 +48,15 @@ func TestRenderJSON(t *testing.T) {
 			&SQSJobSummary{ID:"1",Payload:"p1",StartAt:10},
 		},
 	})
-	if len(w.ResBytes) != len(`{"current_jobs":[{"id":"1","payload":"p1","start_at":10}]}`) {
-		t.Errorf("response body failed: %s", w.ResponseString())
+	var r SQSStatCurrentJobsResponse
+	if err := json.Unmarshal(w.ResBytes, &r); err != nil {
+		t.Error("json unmarshal error", err)
+	}
+	if len(r.CurrentJobs) != 1 {
+		t.Error("current_jobs count invalid")
+	}
+	if r.CurrentJobs[0].ID != "1" {
+		t.Error("job id invalid")
 	}
 }
 
@@ -54,4 +65,93 @@ func TestNewStatHandler(t *testing.T) {
 	if h == nil {
 		t.Error("stat handler not loaded.")
 	}
+}
+
+func TestWorkerCurrentSummaryAndJobsHandler(t *testing.T) {
+	tr := NewJobTracker(5)
+	h := NewStatHandler(tr)
+
+	for i := 1; i <= tr.MaxProcessCount; i++ {
+		j := &SQSJob{
+			Msg: &sqs.Message{
+				MessageId: aws.String("id:" + strconv.Itoa(i)),
+				Body:      aws.String(`foobar`),
+			},
+		}
+		tr.Add(j)
+	}
+
+	summaryController := h.WorkerCurrentSummaryHandler()
+	req := &http.Request{}
+	req.Method = "POST"	
+
+	t.Run("invalid Method for summary", func(t *testing.T) {
+		w := NewSQSMockResponseWriter()
+		summaryController(w, req)
+
+		if w.StatusCode == http.StatusOK {
+			t.Error("response error found")
+		}
+	})
+
+	t.Run("valid Method for summary", func(t *testing.T) {
+		w := NewSQSMockResponseWriter()
+		req.Method = "GET"
+		summaryController(w, req)
+
+		if w.StatusCode != http.StatusOK {
+			t.Error("response error found")
+		}
+
+		var r SQSStatCurrentSummaryResponse
+		if err := json.Unmarshal(w.ResBytes, &r); err != nil {
+			t.Error("json unmarshal error", err)
+		}
+
+		if r.JobsCount != tr.MaxProcessCount {
+			t.Error("job summaries invalid")
+		}
+
+		if r.RestCount != 0 {
+			t.Error("rest count invalid")
+		}
+	})
+
+	jobsController := h.WorkerCurrentJobsHandler()
+	req.Method = "POST"
+
+	t.Run("invalid Method for jobs", func(t *testing.T) {
+		w := NewSQSMockResponseWriter()
+		jobsController(w, req)
+
+		if w.StatusCode == http.StatusOK {
+			t.Error("response error found")
+		}
+	})
+
+	t.Run("valid Method for jobs", func(t *testing.T) {
+		w := NewSQSMockResponseWriter()
+		req.Method = "GET"
+		jobsController(w, req)
+
+		if w.StatusCode != http.StatusOK {
+			t.Error("response error found")
+		}
+
+		var r SQSStatCurrentJobsResponse
+		if err := json.Unmarshal(w.ResBytes, &r); err != nil {
+			t.Error("json unmarshal error", err)
+		}
+
+		if len(r.CurrentJobs) != tr.MaxProcessCount {
+			t.Error("current_jobs count invalid")
+		}
+
+		for _, summary := range r.CurrentJobs {
+			if _, exists := tr.CurrentWorkings[summary.ID]; !exists {
+				t.Errorf("job summary not registered: %s\n", summary.ID)
+			}
+		}
+	})
+
 }
