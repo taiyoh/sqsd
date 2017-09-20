a17baff79293237738924042d330f62c836e23db
taiyoh <sun.basix@gmail.com>
2017-09-20 17:04:28 +0900

    sync.RWMutexでrace condition対策を入れる

--- job_tracker.go
+++ job_tracker.go
@@ -1,9 +1,14 @@
 package sqsd
 
+import (
+	"sync"
+)
+
 type JobTracker struct {
 	CurrentWorkings map[string]*Job
 	MaxProcessCount int
 	JobWorking      bool
+	mu              *sync.RWMutex
 }
 
 func NewJobTracker(maxProcCount uint) *JobTracker {
@@ -11,26 +16,34 @@ func NewJobTracker(maxProcCount uint) *JobTracker {
 		CurrentWorkings: make(map[string]*Job),
 		MaxProcessCount: int(maxProcCount),
 		JobWorking:      true,
+		mu:              &sync.RWMutex{},
 	}
 }
 
 func (t *JobTracker) Add(job *Job) bool {
+	t.mu.Lock()
 	if len(t.CurrentWorkings) >= t.MaxProcessCount {
+		t.mu.Unlock()
 		return false
 	}
 	t.CurrentWorkings[job.ID()] = job
+	t.mu.Unlock()
 	return true
 }
 
 func (t *JobTracker) Delete(job *Job) {
+	t.mu.Lock()
 	delete(t.CurrentWorkings, job.ID())
+	t.mu.Unlock()
 }
 
 func (t *JobTracker) CurrentSummaries() []*JobSummary {
 	currentList := []*JobSummary{}
+	t.mu.RLock()
 	for _, job := range t.CurrentWorkings {
 		currentList = append(currentList, job.Summary())
 	}
+	t.mu.RUnlock()
 	return currentList
 }
 
