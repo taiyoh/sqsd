31ab60f6bd2a05013b7998b35c97ba8fda9adaf4
taiyoh <sun.basix@gmail.com>
2017-09-20 17:26:44 +0900

    serverの取り回し方を勘違いしてたっぽい

--- stat_server.go
+++ stat_server.go
@@ -26,17 +26,28 @@ func (s *StatServer) Run(ctx context.Context, wg *sync.WaitGroup) {
 	defer wg.Done()
 	log.Println("stat server start.")
 
-	wg.Add(1)
+	syncWait := &sync.WaitGroup{}
+	syncWait.Add(1)
 	go func() {
-		defer wg.Done()
+		defer syncWait.Done()
 		if err := s.Srv.ListenAndServe(); err != http.ErrServerClosed {
 			log.Fatal(err)
 		}
 	}()
-
-	if err := s.Srv.Shutdown(ctx); err != nil {
-		log.Fatal(err)
-	}
+	syncWait.Add(1)
+	go func () {
+		defer syncWait.Done()
+		for {
+			select {
+			case <- ctx.Done():
+				if err := s.Srv.Shutdown(ctx); err != nil {
+					log.Fatal(err)
+				}
+				return
+			}
+		}
+	}()
+	syncWait.Wait()
 
 	log.Println("stat server closed.")
 }
