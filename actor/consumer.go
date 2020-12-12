5b36cbf13825d807dc72e37242f5b0b5edfcce12
taiyoh <sun.basix@gmail.com>
2020-12-12 19:57:09 +0900

    feat: add consumer actor

---
+++ actor/consumer.go
@@ -0,0 +1,125 @@
+package sqsd
+
+import (
+	"context"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+)
+
+type Task struct {
+	id        string
+	receipt   string
+	startedAt time.Time
+}
+
+// Consumer manages Invoker's invokation from receiving queues.
+type Consumer struct {
+	mu      sync.RWMutex
+	working map[string]*Task
+	stack   chan struct{}
+	invoker Invoker
+	gateway *actor.PID
+	ctx     context.Context
+	cancel  context.CancelFunc
+}
+
+// NewConsumer returns Consumer actor Prop.
+func NewConsumer(invoker Invoker, gateway *actor.PID, parallel int) *Consumer {
+	return &Consumer{
+		invoker: invoker,
+		gateway: gateway,
+		stack:   make(chan struct{}, parallel),
+		working: make(map[string]*Task),
+	}
+}
+
+func (csm *Consumer) NewQueueActor() *actor.Props {
+	return actor.PropsFromFunc(csm.queueReceiver)
+}
+
+func (csm *Consumer) NewMonitorActor() *actor.Props {
+	return actor.PropsFromFunc(csm.monitoringReceiver)
+}
+
+// PostQueue represents message that Consumer receives when Queue posted.
+type PostQueue struct {
+	Queue Queue
+}
+
+func (csm *Consumer) start() {
+	csm.mu.Lock()
+	defer csm.mu.Unlock()
+	csm.ctx, csm.cancel = context.WithCancel(context.Background())
+}
+
+func (csm *Consumer) stop() {
+	csm.mu.Lock()
+	defer csm.mu.Unlock()
+	csm.cancel()
+	csm.cancel = nil
+}
+
+type CurrentWorkingsMessage struct{}
+
+func (csm *Consumer) monitoringReceiver(c actor.Context) {
+	switch c.Message().(type) {
+	case *CurrentWorkingsMessage:
+		csm.mu.Lock()
+		defer csm.mu.Unlock()
+		tasks := make([]*Task, 0, len(csm.working))
+		for _, tsk := range csm.working {
+			tasks = append(tasks, tsk)
+		}
+		sort.Slice(tasks, func(i, j int) bool {
+			return tasks[i].startedAt.Before(tasks[j].startedAt)
+		})
+		c.Respond(tasks)
+	}
+}
+
+func (csm *Consumer) queueReceiver(c actor.Context) {
+	switch x := c.Message().(type) {
+	case *actor.Started:
+		csm.start()
+	case *actor.Stopping:
+		csm.stop()
+	case *PostQueue:
+		csm.setup(x.Queue)
+		go func(q Queue) {
+			defer csm.free(q)
+			switch csm.invoker.Invoke(csm.ctx, q) {
+			case nil:
+				c.Send(csm.gateway, &RemoveQueueMessage{
+					Queue:  q,
+					Sender: c.Self(),
+				})
+				return
+			default:
+				// TODO: logging
+				return
+			}
+		}(x.Queue)
+		c.Respond(struct{}{})
+	}
+}
+
+func (csm *Consumer) setup(q Queue) {
+	csm.stack <- struct{}{}
+	csm.mu.Lock()
+	defer csm.mu.Unlock()
+	csm.working[q.ID] = &Task{
+		id:        q.ID,
+		receipt:   q.Receipt,
+		startedAt: time.Now(),
+	}
+}
+
+func (csm *Consumer) free(q Queue) {
+	csm.mu.Lock()
+	defer csm.mu.Unlock()
+	delete(csm.working, q.ID)
+	<-csm.stack
+}
---
+++ actor/consumer_test.go
@@ -0,0 +1,62 @@
+package sqsd
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/stretchr/testify/assert"
+)
+
+type testInvoker func(context.Context, Queue) error
+
+func (f testInvoker) Invoke(ctx context.Context, q Queue) error {
+	return f(ctx, q)
+}
+
+type dummyGatewayActor struct {
+}
+
+func (a *dummyGatewayActor) Receive(c actor.Context) {
+}
+
+func TestConsumer(t *testing.T) {
+	sys := actor.NewActorSystem()
+
+	rcvCh := make(chan Queue, 100)
+	nextCh := make(chan struct{}, 100)
+
+	testInvokerFn := func(ctx context.Context, q Queue) error {
+		rcvCh <- q
+		<-nextCh
+		return nil
+	}
+
+	gw := &dummyGatewayActor{}
+	gateway := sys.Root.Spawn(actor.PropsFromFunc(gw.Receive))
+	consumer := NewConsumer(testInvoker(testInvokerFn), gateway, 1)
+	queueActor := sys.Root.Spawn(consumer.NewQueueActor())
+	monitorActor := sys.Root.Spawn(consumer.NewMonitorActor())
+
+	go func() {
+		for i := 1; i <= 8; i++ {
+			q := Queue{
+				ID:      fmt.Sprintf("queue_id_%d", i),
+				Receipt: fmt.Sprintf("queue_receipt_%d", i),
+			}
+			sys.Root.RequestFuture(queueActor, &PostQueue{Queue: q}, -1).Wait()
+		}
+	}()
+
+	for i := 1; i <= 8; i++ {
+		q := <-rcvCh
+		res, err := sys.Root.RequestFuture(monitorActor, &CurrentWorkingsMessage{}, -1).Result()
+		assert.NoError(t, err)
+		tasks, ok := res.([]*Task)
+		assert.True(t, ok)
+		assert.Len(t, tasks, 1)
+		assert.Equal(t, tasks[0].id, q.ID)
+		nextCh <- struct{}{}
+	}
+}
