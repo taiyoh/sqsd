aed33dc8181417355363822698b68d6f9db82088
taiyoh <sun.basix@gmail.com>
2020-12-27 16:58:01 +0900

    fix: set log level from enviroment variable

--- cmd/sqsd-actor/main.go
+++ cmd/sqsd-actor/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"os"
 	"os/signal"
 	"strconv"
@@ -10,42 +9,62 @@ import (
 	"time"
 
 	"github.com/AsynkronIT/protoactor-go/actor"
+	"github.com/AsynkronIT/protoactor-go/log"
 	"github.com/AsynkronIT/protoactor-go/mailbox"
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/sqs"
 	sqsd "github.com/taiyoh/sqsd/actor"
 )
 
 type args struct {
-	rawURL   string
-	queueURL string
-	dur      time.Duration
-	parallel int
+	endpoint        string
+	rawURL          string
+	queueURL        string
+	dur             time.Duration
+	fetcherParallel int
+	invokerParallel int
+	logLevel        log.Level
 }
 
 func main() {
 	as := actor.NewActorSystem()
 
 	args := parse()
+	sqsd.SetLogLevel(args.logLevel)
 
 	ivk, err := sqsd.NewHTTPInvoker(args.rawURL, args.dur)
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	queue := sqs.New(session.Must(session.NewSession()))
+	queue := sqs.New(
+		session.Must(session.NewSession()),
+		aws.NewConfig().WithEndpoint(args.endpoint),
+	)
 
-	f := sqsd.NewFetcher(queue, args.queueURL, args.parallel)
-	r := sqsd.NewRemover(queue, args.queueURL, args.parallel)
+	f := sqsd.NewFetcher(queue, args.queueURL, args.fetcherParallel)
+	r := sqsd.NewRemover(queue, args.queueURL, args.fetcherParallel)
 
 	fetcher := as.Root.Spawn(f.NewBroadcastPool())
 	remover := as.Root.Spawn(r.NewRoundRobinGroup().
-		WithMailbox(mailbox.Bounded(args.parallel * 100)))
+		WithMailbox(mailbox.Bounded(args.fetcherParallel * 100)))
 
-	c := sqsd.NewConsumer(ivk, remover, args.parallel)
+	c := sqsd.NewConsumer(ivk, remover, args.fetcherParallel)
 	consumer := as.Root.Spawn(c.NewQueueActorProps().
-		WithMailbox(mailbox.Bounded(args.parallel + 10)))
+		WithMailbox(mailbox.Bounded(args.fetcherParallel + 10)))
 	monitor := as.Root.Spawn(c.NewMonitorActorProps())
+
+	logger := log.New(args.logLevel, "[sqsd-main]")
+
+	logger.Info("start process")
+	logger.Info("queue settings",
+		log.String("url", args.queueURL),
+		log.Int("parallel", args.fetcherParallel))
+	logger.Info("invoker settings",
+		log.String("url", args.rawURL),
+		log.Int("parallel", args.invokerParallel),
+		log.Duration("timeout", args.dur))
 
 	as.Root.Send(fetcher, &sqsd.StartGateway{
 		Sender: consumer,
@@ -58,12 +77,14 @@ func main() {
 	wg.Add(1)
 	go func() {
 		defer wg.Done()
-		<-sigCh
+		sig := <-sigCh
+		logger.Info("signal caught. stopping worker...", log.Object("signal", sig))
 		as.Root.Send(fetcher, &sqsd.StopGateway{})
 		for {
 			res, err := as.Root.RequestFuture(monitor, &sqsd.CurrentWorkingsMessage{}, -1).Result()
 			if err != nil {
-				log.Fatal(err)
+				logger.Error("failed to retrieve current_workings", log.Error(err))
+				panic(err)
 			}
 			if tasks := res.([]*sqsd.Task); len(tasks) == 0 {
 				return
@@ -73,19 +94,40 @@ func main() {
 	}()
 
 	wg.Wait()
+
+	logger.Info("end process")
 }
 
 func parse() args {
+	endpoint := os.Getenv("SQS_ENDPOINT")
 	rawURL := mustGetenv("INVOKER_URL")
 	queueURL := mustGetenv("QUEUE_URL")
 	defaultTimeOutSeconds := defaultIntGetEnv("DEFAULT_INVOKER_TIMEOUT_SECONDS", 60)
-	parallel := defaultIntGetEnv("PARALLEL_COUNT", 1)
+	fetcherParallel := defaultIntGetEnv("FETCHER_PARALLEL_COUNT", 1)
+	invokerParallel := defaultIntGetEnv("INVOKER_PARALLEL_COUNT", 1)
+
+	levelMap := map[string]log.Level{
+		"debug": log.DebugLevel,
+		"info":  log.InfoLevel,
+		"error": log.ErrorLevel,
+	}
+	l := log.InfoLevel
+	if ll, ok := os.LookupEnv("LOG_LEVEL"); ok {
+		lll, ok := levelMap[ll]
+		if !ok {
+			panic("invalid LOG_LEVEL")
+		}
+		l = lll
+	}
 
 	return args{
-		rawURL:   rawURL,
-		queueURL: queueURL,
-		dur:      time.Duration(defaultTimeOutSeconds) * time.Second,
-		parallel: parallel,
+		endpoint:        endpoint,
+		rawURL:          rawURL,
+		queueURL:        queueURL,
+		dur:             time.Duration(defaultTimeOutSeconds) * time.Second,
+		fetcherParallel: fetcherParallel,
+		invokerParallel: invokerParallel,
+		logLevel:        l,
 	}
 }
 
