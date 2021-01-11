0609798021ad30463bed021b931ffd88ed5d83e5
taiyoh <sun.basix@gmail.com>
2021-01-11 11:25:22 +0900

    fix: set LogLevel using encoding.UnmarshalText

--- cmd/sqsd-actor/main.go
+++ cmd/sqsd-actor/main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"path/filepath"
-	"reflect"
 	"syscall"
 	"time"
 
@@ -27,13 +25,17 @@ type config struct {
 	FetcherParallel int           `env:"FETCHER_PARALLEL_COUNT" envDefault:"1"`
 	InvokerParallel int           `env:"INVOKER_PARALLEL_COUNT" envDefault:"1"`
 	MonitoringPort  int           `env:"MONITORING_PORT" envDefault:"6969"`
-	LogLevel        palog.Level   `env:"LOG_LEVEL" envDefault:"info"`
+	LogLevel        sqsd.LogLevel `env:"LOG_LEVEL" envDefault:"info"`
 }
 
 func main() {
 	loadEnvFromFile()
 
-	args := parse()
+	args := config{}
+	if err := env.Parse(&args); err != nil {
+		log.Fatal(err)
+	}
+
 	sqsd.SetLogLevel(args.LogLevel)
 
 	queue := sqs.New(
@@ -56,7 +58,16 @@ func main() {
 		MonitoringPort:    args.MonitoringPort,
 	})
 
-	logger := initLogger(args)
+	logger := palog.New(args.LogLevel.Level, "[sqsd-main]")
+
+	logger.Info("start process")
+	logger.Info("queue settings",
+		palog.String("url", args.QueueURL),
+		palog.Int("parallel", args.FetcherParallel))
+	logger.Info("invoker settings",
+		palog.String("url", args.RawURL),
+		palog.Int("parallel", args.InvokerParallel),
+		palog.Duration("timeout", args.Duration))
 
 	if err := sys.Start(); err != nil {
 		log.Fatal(err)
@@ -76,31 +87,6 @@ func main() {
 	time.Sleep(500 * time.Millisecond)
 }
 
-var logParser env.ParserFunc = func(v string) (interface{}, error) {
-	levelMap := map[string]palog.Level{
-		"debug": palog.DebugLevel,
-		"info":  palog.InfoLevel,
-		"warn":  palog.WarnLevel,
-		"error": palog.ErrorLevel,
-	}
-	l, ok := levelMap[v]
-	if !ok {
-		return 0, fmt.Errorf("invalid LOG_LEVEL: %s", v)
-	}
-	return l, nil
-}
-
-func parse() config {
-	cfg := config{}
-	funcMap := map[reflect.Type]env.ParserFunc{
-		reflect.TypeOf(palog.InfoLevel): logParser,
-	}
-	if err := env.ParseWithFuncs(&cfg, funcMap); err != nil {
-		log.Fatal(err)
-	}
-	return cfg
-}
-
 var cwd, _ = os.Getwd()
 
 func loadEnvFromFile() {
@@ -118,19 +104,4 @@ func loadEnvFromFile() {
 	if err := godotenv.Load(fp); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func initLogger(args config) *palog.Logger {
-	logger := palog.New(args.LogLevel, "[sqsd-main]")
-
-	logger.Info("start process")
-	logger.Info("queue settings",
-		palog.String("url", args.QueueURL),
-		palog.Int("parallel", args.FetcherParallel))
-	logger.Info("invoker settings",
-		palog.String("url", args.RawURL),
-		palog.Int("parallel", args.InvokerParallel),
-		palog.Duration("timeout", args.Duration))
-
-	return logger
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.14
 replace github.com/taiyoh/sqsd/actor => ./actor
 
 require (
-	github.com/AsynkronIT/protoactor-go v0.0.0-20201225111938-2a1b4e9f6793
+	github.com/AsynkronIT/protoactor-go v0.0.0-20210104230532-90c8f2d201d8
 	github.com/aws/aws-sdk-go v1.35.35
 	github.com/caarlos0/env/v6 v6.4.0
 	github.com/fukata/golang-stats-api-handler v1.0.0
