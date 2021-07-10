bf1434f163bc3d8ebbbcd87f3aee002f08ee3462
taiyoh <sun.basix@gmail.com>
2021-07-10 21:26:10 +0900

    feat: add QueueLocker interface
    and add memory QueueLocker

--- actor/go.mod
+++ actor/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/aws/aws-sdk-go v1.35.35
 	github.com/caarlos0/env/v6 v6.4.0
 	github.com/golang/protobuf v1.4.3
-	github.com/stretchr/testify v1.6.1
+	github.com/stretchr/testify v1.7.0
 	google.golang.org/grpc v1.34.0
 	google.golang.org/protobuf v1.25.0
 )
---
+++ actor/locker_test.go
@@ -0,0 +1,62 @@
+package sqsd
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMemoryLocker(t *testing.T) {
+	l := NewMemoryQueueLocker()
+	ctx := context.Background()
+
+	assert.NoError(t, l.Lock(ctx, "hogefuga"))
+	assert.NoError(t, l.Lock(ctx, "foobarbaz"))
+	assert.ErrorIs(t, l.Lock(ctx, "foobarbaz"), ErrQueueExists)
+
+	ids, err := l.Find(ctx, time.Now())
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"foobarbaz", "hogefuga"}, ids)
+
+	ids, err = l.Find(ctx, time.Now().Add(expireDur))
+	assert.NoError(t, err)
+	assert.Empty(t, ids)
+
+	assert.NoError(t, l.Unlock(ctx, "aiueo"))
+	ids, err = l.Find(ctx, time.Now())
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"foobarbaz", "hogefuga"}, ids)
+	assert.NoError(t, l.Unlock(ctx, "foobarbaz"))
+	ids, err = l.Find(ctx, time.Now())
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"hogefuga"}, ids)
+	assert.NoError(t, l.Unlock(ctx, "hogefuga"))
+	ids, err = l.Find(ctx, time.Now())
+	assert.NoError(t, err)
+	assert.Empty(t, ids)
+}
+
+func TestRunQueueLocker(t *testing.T) {
+	l := NewMemoryQueueLocker(MemoryLockerDuration(50 * time.Millisecond))
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go RunQueueLocker(ctx, l, 30*time.Millisecond)
+
+	assert.NoError(t, l.Lock(ctx, "hooooooooo"))
+	time.Sleep(25 * time.Millisecond)
+	assert.NoError(t, l.Lock(ctx, "baaaaaaaaa"))
+
+	for _, refs := range [][]string{
+		{
+			"baaaaaaaaa", "hooooooooo",
+		},
+		{},
+	} {
+		ids, _ := l.Find(ctx, time.Now().UTC())
+		assert.Equal(t, refs, ids)
+		time.Sleep(30 * time.Millisecond)
+	}
+}
